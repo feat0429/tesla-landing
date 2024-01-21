15f1f2dd0f733f2fe0365944e1db457b90823626
Fredy Amaya <feat0429@gmail.com>
2024-01-21 12:13:43 -0600

    Added section content animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,13 +9,23 @@ export default {
 			colors:{
 				vulcan:'#393c41',
 				whiteSmoke: '#f4f4f4',
-				blackRussian: '#171a20'							
+				blackRussian: '#171a20',
+				whisper:'#eeeeee'							
 			},
 			boxShadow:{
 				dark: '#171a20 0px 1px 0px 0px',
 				light: 'white 0px 1px 0px 0px',
 				darkHover: '#171a20 0px 2px 0px 0px',
-				lightHover: 'white 0px 2px 0px 0px'
+				lightHover: 'white 0px 2px 0px 0px',
+			},
+			animation:{
+				fadeIn: ' 0.5s ease-in-out fadeIn'
+			},
+			keyframes:{
+				fadeIn:{
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 }
+				}
 			}
 		},
 	},
